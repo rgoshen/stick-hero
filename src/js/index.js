09f80d88468d46edfc02acb4751e4543916720d1
Richard Goshen <rick.goshen@gmail.com>
2023-11-21 09:55:31 -0700

    stub out initial game logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
-// js scripts goes here
+// Game state
+let phase = 'waiting'; // waiting | stretching | turning | walking | transitioning | falling
+let lastTimestamp;
+let heroX;
+let heroY;
+let screenOffest;
+let platforms = [];
+let sticks = [];
+let score = 0;
+
+// Configuration
+
+// Getting the canvas element
+const canvas = document.getElementById('game');
+
+// Getting the drawing element
+const ctx = canvas.getContext('2d');
+
+// Further UI elements
+const scoreElement = document.getElementById('score');
+const restartButton = document.getElementById('restart');
+
+// Start game
+resetGame();
+
+// Resets game state and layout
+function resetGame() {
+  draw();
+}
+
+function draw() {}
+
+window.addEventListener('mousedown', (e) => {});
+window.addEventListener('mouseup', (e) => {});
+
+function animate(timeStamp) {}
